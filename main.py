d60284068c153903a26a4508cda6299aa1074e41
suhaib <suhaibvbm@gmail.com>
2023-11-10 10:50:15 +0530

    exercise finished

--- main.py
+++ main.py
@@ -15,10 +15,22 @@
 #     # print("file was closed")
 #     raise TypeError("this is an error that i made up")   #error we can create
 
-height = float(input("Height: "))
-weight = float(input("weight: "))
+# height = float(input("Height: "))
+# weight = float(input("weight: "))
+#
+# if height > 3:
+#     raise ValueError("human height should not over 3 meter")
+# bmi = weight / height ** 2
+# print(bmi)
 
-if height > 3:
-    raise ValueError("human height should not over 3 meter")
-bmi = weight / height ** 2
-print(bmi)
+facebook_posts = [{'Likes': 21, 'Comments': 2}, {'Likes': 13, 'Comments': 2, 'Shares': 1}, {'Likes': 33, 'Comments': 8, 'Shares': 3}, {'Comments': 4, 'Shares': 2},{'Comments': 1, 'Shares': 1}, {'Likes': 19, 'Comments': 3}]
+
+total_likes = 0
+# Catching the KeyError exception in the dictionary
+for post in facebook_posts:
+  try:
+    total_likes = total_likes + post['Likes']
+  except KeyError:
+    pass
+
+print(total_likes)
